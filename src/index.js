0d3380a123d698467df26bd7f8a83f9d11e5ee76
Xavier Garcia Buils <xgbuils@gmail.com>
2016-08-10 21:42:36 +0200

    add behaviour when get is called at first time

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ function Schrodinger (list) {
     if (!_.isFunction(list) && !_.isArray(list)) {
         throw new Error('`' + list + '` must be a function or a list')
     }
+
+    this.list = list
+}
+
+Schrodinger.prototype.get = function (seed) {
+    var list = this.list
+    return _.isFunction(this.list) ? list(seed) : list[seed % list.length]
 }
 
 module.exports = Schrodinger
--- test/test.js
+++ test/test.js
@@ -33,4 +33,26 @@ describe('schrodinger', function () {
             expect(test).to.throw('`100` must be a function or a list')
         })
     })
+
+    describe('calling get at first time', function () {
+        describe('after array constructor parameter', function () {
+            it('if is passed a seed, it returns element in seed % array.length position', function () {
+                var array = [23, 'foo', {}]
+                var seed = 16
+                var schrodinger = new Schrodinger(array)
+                expect(schrodinger.get(seed)).to.be.equal(array[1])
+            })
+        })
+
+        describe('after function constructor parameter', function () {
+            it('if is passed a seed, it returns the same value that returns the function parameter', function () {
+                var fn = function (seed) {
+                    return seed * seed + 24
+                }
+                var seed = 5
+                var schrodinger = new Schrodinger(fn)
+                expect(schrodinger.get(seed)).to.be.equal(fn(seed))
+            })
+        })
+    })
 })
